c5e07aec4b84e17c3abf07d8a0cc8c1ad2f17669
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-17 16:13:27 +0800

    Add the main function to creat dictionary of emails and names

---
+++ emails.py
@@ -0,0 +1,13 @@
+def main():
+    """Creat a dictionary of emails and names"""
+    email_and_name = {}
+    while email != "":
+        name = get_name(email)
+        confirmation = input("Is your name {}? (Y/n)".format(name))
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_and_name[email] = name
+        email = input("Email: ")
+
+
+main()
