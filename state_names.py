304b52ee7576b05908a7803da41d94f3679a457e
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-17 15:41:59 +0800

    Add a loop to print the state names neatly

--- state_names.py
+++ state_names.py
@@ -10,3 +10,7 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
+
+# a loop that displays all of the states and names neatly lined up
+for state in CODE_TO_NAME:
+    print(f"{state:3} is {CODE_TO_NAME[state]}")
