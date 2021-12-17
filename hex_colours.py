5a8395978dfb55a84972e59b4e85dfb744ddfb55
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-17 15:59:00 +0800

    Add a program to show the colours' code with error checking when there is an user's input

--- hex_colours.py
+++ hex_colours.py
@@ -3,6 +3,14 @@ COLOUR_CODES = {"Aqua": "00ffff", "Aquamarine1": "7fffd4",
                 "Armygreen": "4b5320", "Arylideyellow": "e9d66b",
                 "Ashgrey": "b2beb5", "Asparagus": "87a96b",
                 "Aureolin": "fdee00", "Azure1": "f0ffff"}
-
+colour_name = input("Please enter a colour name: ")
+while 1:
+    if colour_name in COLOUR_CODES:
+        print(f"The code for {colour_name} is {COLOUR_CODES[colour_name]}.")
+    elif colour_name == "":
+        break
+    else:
+        print("Invalid input")
+    colour_name = input("Please enter a colour name: ")
 
 
