980ae2e7e833714912457d6089afb55599f0ce82
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-17 16:07:12 +0800

    Add a program to display the words count

--- word_occurrences.py
+++ word_occurrences.py
@@ -4,3 +4,7 @@ words = text.split()
 for word in words:
     frequency = word_counts.get(word, 0)
     word_counts[word] = frequency + 1
+words = list(word_counts.keys())
+max_len = max((len(word) for word in words))
+for word in words:
+    print(f"{word} : {word_counts[word]}")
