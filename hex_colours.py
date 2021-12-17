c9249e1b4f6cc32991bd77b1ebd244fadb0e94e8
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-17 15:54:14 +0800

    Add 10 colour codes

---
+++ hex_colours.py
@@ -0,0 +1,8 @@
+COLOUR_CODES = {"Aqua": "00ffff", "Aquamarine1": "7fffd4",
+                "Aquamarine2": "76eec6", "Aquamarine4": "458b74",
+                "Armygreen": "4b5320", "Arylideyellow": "e9d66b",
+                "Ashgrey": "b2beb5", "Asparagus": "87a96b",
+                "Aureolin": "fdee00", "Azure1": "f0ffff"}
+
+
+
